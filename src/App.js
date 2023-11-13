a407ac917795090e39c6cf9b47d1569d67f918ce
jonique98 <tnals9899@naver.com>
2023-11-14 02:52:09 +0900

    feat: Planner를 통해서 각 객체를 초기화 하고, 에러를 캐치해서 다시 입력을 받는 메소드 입출력 메소드를 import해서 입력값으로 Planner의 메소드 인자로 활용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import InputView from './view/InputView.js';
+import OutputView from './view/OutputView.js';
+import Planner from './controller/Planner.js';
+
 class App {
-  async run() {}
+	async run() {
+		const planner = new Planner();
+		await this.readDate(planner);
+		await this.readOrder(planner);
+		this.printplanner(planner);
+	}
+
+	async readDate(planner) {
+		// eslint-disable-next-line no-constant-condition
+		while (true) {
+			try {
+				// eslint-disable-next-line no-await-in-loop
+				const date = await InputView.readDate();
+				planner.initDate(date);
+				break;
+			} catch (error) {
+				OutputView.printDateError();
+			}
+		}
+	}
+
+	async readOrder(planner) {
+		// eslint-disable-next-line no-constant-condition
+		while (true) {
+			try {
+				// eslint-disable-next-line no-await-in-loop
+				const orderString = await InputView.readOrder();
+				planner.takeOrder(orderString);
+				break;
+			} catch (error) {
+				OutputView.printOrderError();
+			}
+		}
+	}
 }
 
 export default App;
