ad581e49ac27a6bf7eadda061c911afdd4c1c1d2
jonique98 <tnals9899@naver.com>
2023-11-14 02:12:13 +0900

    feat: date의 유효성을 검사하는 validate 메소드 추가 1. 숫자인지 검사 2. 1과 31 사이인지 검사

--- src/model/Date.js
+++ src/model/Date.js
@@ -7,6 +7,18 @@ class Date {
 		this.#validate(date);
 		this.#date = Number(date);
 	}
+
+	#validate(date) {
+		const dateToNumber = Number(date);
+
+		if (Number.isNaN(dateToNumber)) {
+			throw new Error();
+		}
+
+		if (dateToNumber < 1 || dateToNumber > 31) {
+			throw new Error();
+		}
+	}
 }
 
 export default Date;
