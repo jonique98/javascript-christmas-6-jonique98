fecb6e10263a8f2134478777d7607e9b248fbcd8
jonique98 <tnals9899@naver.com>
2023-11-14 02:31:58 +0900

    feat: menu에 대한 validate 검사 메소드 1. 중복된 메뉴 검사 2. 메뉴 20개 초과 검사

--- src/model/OrderMenu.js
+++ src/model/OrderMenu.js
@@ -8,4 +8,15 @@ class OrderMenu {
 		this.#validateMenu(menu);
 		this.#menu = menu;
 	}
+
+	#validateMenu(menu) {
+		const categories = new Set(Object.keys(menu).map(food => Utils.findFoodCategory(food)));
+
+		if (categories.size === 1 && categories.has('음료')) {
+			throw new Error('모든 주문 음료');
+		}
+
+		const totalItemCount = Object.values(menu).reduce((total, count) => total + count, 0);
+		if (totalItemCount > 20) throw new Error('주문 20개 초과');
+	}
 }
