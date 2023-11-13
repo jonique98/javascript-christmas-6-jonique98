f1ab3f840efd83d739f9c7684db849afadaacdb7
jonique98 <tnals9899@naver.com>
2023-11-14 02:45:40 +0900

    feat: 혜택과 관련된 메소드 모두 구현, 각 메소드가 하나의 혜택을 반환 요구사항에서 말하는 모든 혜택과 1대1로 대응하는 혜택 메소드 선언 각 객체의 메세지들을 활용하고 문자열을 만들어서 반환

--- src/controller/Planner.js
+++ src/controller/Planner.js
@@ -15,4 +15,56 @@ class Planner {
 	takeOrder(orderString) {
 		this.#order = new Order(orderString);
 	}
+
+	benefits() {
+		this.#benefits = new Benefits();
+		if (!this.#order.canGetBenefit()) return [SIGN.noBenefit];
+
+		const totalBenefits = [];
+
+		totalBenefits.push(this.dailyBenefit());
+		totalBenefits.push(this.christmasBenefit());
+		totalBenefits.push(this.specialBenefit());
+		totalBenefits.push(this.giveawayBenefit());
+
+		if (totalBenefits.every(benefit => benefit === SIGN.emptyString)) return [SIGN.noBenefit];
+		return totalBenefits;
+	}
+
+	dailyBenefit() {
+		let dailyBenefit = 0;
+
+		if (this.#date.dateIsWeekend()) {
+			dailyBenefit = this.#benefits.weekendDiscount(this.#order.mainMenuNumber());
+			if (dailyBenefit === 0) return SIGN.emptyString;
+			return `${BENEFITMESSAGE.weekend}${dailyBenefit.toLocaleString()}${SIGN.moneyPreffix}`;
+		}
+
+		dailyBenefit = this.#benefits.weekdayDiscount(this.#order.desertMenuNumber());
+		if (dailyBenefit === 0) return SIGN.emptyString;
+		return `${BENEFITMESSAGE.weekday}${dailyBenefit.toLocaleString()}${SIGN.moneyPreffix}`;
+	}
+
+	christmasBenefit() {
+		if (!this.#date.dateIsBeforeChristmas()) return SIGN.emptyString;
+		const christmasBenefit = this.#benefits.christmasDiscount(this.#date.christmasLeftDate());
+		return `${BENEFITMESSAGE.christmas}${christmasBenefit.toLocaleString()}${SIGN.moneyPreffix}`;
+	}
+
+	specialBenefit() {
+		const specialBenefit = this.#benefits.specialDiscount(this.#date.dateIsSpecialDay());
+		if (specialBenefit === 0) return SIGN.emptyString;
+		return `${BENEFITMESSAGE.special}${specialBenefit.toLocaleString()}${SIGN.moneyPreffix}`;
+	}
+
+	giveawayBenefit() {
+		const giveawayBenefit = this.#benefits.giveawayEvent(this.#order.canGetGiveaway());
+		if (giveawayBenefit === 0) return SIGN.emptyString;
+		return `${BENEFITMESSAGE.giveaway}${giveawayBenefit.toLocaleString()}${SIGN.moneyPreffix}`;
+	}
+
+	giveawayEvent() {
+		if (this.#order.canGetGiveaway()) return BENEFITMESSAGE.champagne;
+		return SIGN.noBenefit;
+	}
 }
