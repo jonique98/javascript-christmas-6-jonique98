b86d5248ea2a81ba4fbca6859940b8504677a53a
jonique98 <tnals9899@naver.com>
2023-11-15 21:06:33 +0900

    refactor: InpurView 클래스의 readLine 메세지 상수화 INPURMESSAGE 오브젝트를 import해서 상수화

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -34,8 +34,14 @@ const SIGN = {
 };
 
 const ERROR = {
-  date: "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.",
-  order: "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.",
+  date: "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.\n",
+  order: "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.\n",
+};
+
+const INPUTMESSAGE = {
+  date: "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n",
+  order:
+    "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n",
 };
 
 const OUTPUTMESSAGE = {
@@ -90,6 +96,7 @@ export {
   MENU,
   ERROR,
   OUTPUTMESSAGE,
+  INPUTMESSAGE,
   BENEFITMESSAGE,
   SIGN,
   BADGE,
