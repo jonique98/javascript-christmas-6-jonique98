11efe2385228cfbb8145bb2069040cdb063335a4
jonique98 <tnals9899@naver.com>
2023-11-15 15:32:37 +0900

    test: Date 클래스 메소드 테스트 파일 작성 Date 클래스 메소드 테스트 isWeekend 테스트 작성

---
+++ __tests__/DateTest.js
@@ -0,0 +1,16 @@
+import Date from "../src/model/Date.js";
+
+describe("Date", () => {
+  let date;
+
+  test("주말 테스트", () => {
+    // given
+    date = new Date(1);
+
+    // when
+    const isWeekend = date.dateIsWeekend();
+
+    // then
+    expect(isWeekend).toBe(true);
+  });
+});
