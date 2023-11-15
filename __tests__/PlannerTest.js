7278dcbdccb37fceaa80a18c0dadf2742e08b843
jonique98 <tnals9899@naver.com>
2023-11-15 20:36:25 +0900

    test: 주중 할인이 디저트 갯수에 따라 적용되는 지 테스트 디저트 갯수에 따른 주중 할인 적용 테스트

--- __tests__/PlannerTest.js
+++ __tests__/PlannerTest.js
@@ -17,15 +17,14 @@ describe("Planner 클래스 메소드 테스트", () => {
   const specialDayAfterChristmas = "31";
 
   const benefitMenu =
-    "타파스-1,티본스테이크-2,해산물파스타-2,초코케이크-1,레드와인-1";
+    "타파스-3,티본스테이크-3,해산물파스타-3,초코케이크-3,아이스크림-3,레드와인-3";
   const noBenefitMenu = "양송이수프 1,제로콜라 1";
-  const noMainMenu = "양송이수프-2,타파스-2,초코케이크-3,레드와인-5";
-  const noDesertMenu =
-    "해산물파스타-2,티본스테이크-2,크리스마스파스타-2,레드와인-1";
+  const noMainMenu = "타파스-3,초코케이크-3,아이스크림-3,레드와인-3";
+  const noDesertMenu = "타파스-3,티본스테이크-3,해산물파스타-3,레드와인-3";
 
   test("주말 할인 적용 테스트", () => {
     const result = [
-      "주말 할인 -8,092원",
+      "주말 할인 -12,138원",
       "크리스마스 할인 -1,000원",
       "",
       "증정 이벤트 -25,000원",
@@ -51,7 +50,7 @@ describe("Planner 클래스 메소드 테스트", () => {
 
   test("주중 할인 테스트", () => {
     const result = [
-      "평일 할인 -2,023원",
+      "평일 할인 -12,138원",
       "크리스마스 할인 -2,100원",
       "",
       "증정 이벤트 -25,000원",
@@ -59,6 +58,19 @@ describe("Planner 클래스 메소드 테스트", () => {
 
     planner.initDate(weekday);
     planner.takeOrder(benefitMenu);
+    expect(planner.benefits()).toEqual(result);
+  });
+
+  test("디저트 메뉴가 없을 때 주중 할인 미적용 테스트", () => {
+    const result = [
+      "",
+      "크리스마스 할인 -2,100원",
+      "",
+      "증정 이벤트 -25,000원",
+    ];
+
+    planner.initDate(weekday);
+    planner.takeOrder(noDesertMenu);
     expect(planner.benefits()).toEqual(result);
   });
 });
