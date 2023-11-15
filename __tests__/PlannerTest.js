18f9de80f9f5b7ec5aeff177e0737f2f72cafe6e
jonique98 <tnals9899@naver.com>
2023-11-15 20:21:37 +0900

    test: Planner 클래스 테스트 함수 주말 할인이 적절하게 적용되는 지 테스트

---
+++ __tests__/PlannerTest.js
@@ -0,0 +1,38 @@
+import Planner from "../src/Planner/Planner";
+
+describe("Planner 클래스 메소드 테스트", () => {
+  let planner;
+
+  beforeEach(() => {
+    planner = new Planner();
+  });
+
+  const weekend = "1";
+  const weekdayAfterChristmas = "26";
+
+  const weekday = "12";
+  const weekendAfterChristmas = "29";
+
+  const specialDayBeforeChristmas = "25";
+  const specialDayAfterChristmas = "31";
+
+  const benefitMenu =
+    "타파스-1,티본스테이크-2,해산물파스타-2,초코케이크-1,레드와인-1";
+  const noBenefitMenu = "양송이수프 1,제로콜라 1";
+  const noMainMenu = "양송이수프-2,타파스-2,초코케이크-3,레드와인-5";
+  const noDesertMenu =
+    "해산물파스타-2,티본스테이크-2,크리스마스파스타-2,레드와인-1";
+
+  test("주말 할인 적용 테스트", () => {
+    const result = [
+      "주말 할인 -8,092원",
+      "크리스마스 할인 -1,000원",
+      "",
+      "증정 이벤트 -25,000원",
+    ];
+
+    planner.initDate(weekend);
+    planner.takeOrder(benefitMenu);
+    expect(planner.benefits()).toEqual(result);
+  });
+});
