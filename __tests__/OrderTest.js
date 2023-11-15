772f0d71570aa3b7bdf08ee79084f497ccb2abb9
jonique98 <tnals9899@naver.com>
2023-11-15 17:28:16 +0900

    test: 메뉴판에 없는 메뉴를 넣었을 때 error 테스트 메뉴판에 없는 메뉴에 대한 valid 테스트

---
+++ __tests__/OrderTest.js
@@ -0,0 +1,38 @@
+import Order from "../src/model/Order";
+
+describe("Order 클래스 메소드 테스트", () => {
+  let order;
+
+  const invalidInput1 = "해산물파스타2";
+  const invalidInput2 = "해산물파스타 -2";
+  const invalidInput3 = "해산물파스타-2 레드와인-2";
+
+  const invalidMenu = "평양냉면-1";
+  const onlyBeverageMenu = "제로콜라-2,레드와인-1";
+  const moreThanTwentyMenu = "해산물파스타-19,레드와인-2";
+
+  const noBenefitMenu = "양송이수프-1";
+
+  const menu1 = "티본스테이크-1,해산물파스타-1,레드와인-1,초코케이크-1";
+  const menu2 = "양송이수프-1,아이스크림-1,제로콜라-1";
+
+  test("유효하지 않은 형식 validate 테스트", () => {
+    // when
+    const validate1 = () => new Order(invalidInput1);
+    const validate2 = () => new Order(invalidInput2);
+    const validate3 = () => new Order(invalidInput3);
+
+    // then
+    expect(validate1).toThrow();
+    expect(validate2).toThrow();
+    expect(validate3).toThrow();
+  });
+
+  test("메뉴판에 없는 메뉴 validate 테스트", () => {
+    // when
+    const validate = () => new Order(invalidMenu);
+
+    // then
+    expect(validate).toThrow();
+  });
+});
