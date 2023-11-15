3df8149650ab59ab3544dc1baa4345fb28e31adf
jonique98 <tnals9899@naver.com>
2023-11-15 12:25:03 +0900

    fix: import 경로 변경 사항 반영

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
-import InputView from './view/InputView.js';
-import OutputView from './view/OutputView.js';
-import Planner from './controller/Planner.js';
+import InputView from "./view/InputView.js";
+import OutputView from "./view/OutputView.js";
+import Planner from "./Planner/Planner.js";
 
 class App {
-	async run() {
-		const planner = new Planner();
-		await this.readDate(planner);
-		await this.readOrder(planner);
-		this.printplanner(planner);
-	}
+  async run() {
+    const planner = new Planner();
+    await this.readDate(planner);
+    await this.readOrder(planner);
+    this.printplanner(planner);
+  }
 
-	async readDate(planner) {
-		// eslint-disable-next-line no-constant-condition
-		while (true) {
-			try {
-				// eslint-disable-next-line no-await-in-loop
-				const date = await InputView.readDate();
-				planner.initDate(date);
-				break;
-			} catch (error) {
-				OutputView.printDateError();
-			}
-		}
-	}
+  async readDate(planner) {
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      try {
+        // eslint-disable-next-line no-await-in-loop
+        const date = await InputView.readDate();
+        planner.initDate(date);
+        break;
+      } catch (error) {
+        OutputView.printDateError();
+      }
+    }
+  }
 
-	async readOrder(planner) {
-		// eslint-disable-next-line no-constant-condition
-		while (true) {
-			try {
-				// eslint-disable-next-line no-await-in-loop
-				const orderString = await InputView.readOrder();
-				planner.takeOrder(orderString);
-				break;
-			} catch (error) {
-				OutputView.printOrderError();
-			}
-		}
-	}
+  async readOrder(planner) {
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      try {
+        // eslint-disable-next-line no-await-in-loop
+        const orderString = await InputView.readOrder();
+        planner.takeOrder(orderString);
+        break;
+      } catch (error) {
+        OutputView.printOrderError();
+      }
+    }
+  }
 
-	printplanner(planner) {
-		OutputView.printDateEventMessage(planner.dateEventMessage());
-		OutputView.printMenu(planner.orderMenu());
-		OutputView.printPriceBeforeBenefits(planner.priceBeforeBenefits());
-		OutputView.printGiveaway(planner.giveawayEvent());
-		OutputView.printBenefitsList(planner.benefits());
-		OutputView.printBenefitsPrice(planner.totalBenefitsPrice());
-		OutputView.printPriceAfterBenefits(planner.priceAfterBenefits());
-		OutputView.printEventBadge(planner.eventBadge());
-	}
+  printplanner(planner) {
+    OutputView.printDateEventMessage(planner.dateEventMessage());
+    OutputView.printMenu(planner.orderMenu());
+    OutputView.printPriceBeforeBenefits(planner.priceBeforeBenefits());
+    OutputView.printGiveaway(planner.giveawayEvent());
+    OutputView.printBenefitsList(planner.benefits());
+    OutputView.printBenefitsPrice(planner.totalBenefitsPrice());
+    OutputView.printPriceAfterBenefits(planner.priceAfterBenefits());
+    OutputView.printEventBadge(planner.eventBadge());
+  }
 }
 
 export default App;
