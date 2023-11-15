8e55b6171f0aa1a53c5dfcbba9a7c00f95bbddeb
jonique98 <tnals9899@naver.com>
2023-11-15 15:04:56 +0900

    test: 평일 할인 테스트 코드 작성 평일 할인 메소드 반환값 테스트

--- __tests__/BenefitTest.js
+++ __tests__/BenefitTest.js
@@ -13,4 +13,15 @@ describe("Benefits", () => {
     // then
     expect(weekendDiscount).toBe(10115);
   });
+
+  test("평일 할인", () => {
+    // given
+    const desertOrder = 5;
+
+    // when
+    const weekdayDiscount = benefits.weekdayDiscount(desertOrder);
+
+    // then
+    expect(weekdayDiscount).toBe(10115);
+  });
 });
